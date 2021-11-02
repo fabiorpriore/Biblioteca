00c1dbfa9b67cbbf29605507c70c363c76a8dd53
fabio.priore <fabio.priore@escola.pr.gov.br>
2021-11-02 07:42:41 -0300

    upload

--- Models/UsuarioService.cs
+++ Models/UsuarioService.cs
@@ -7,47 +7,58 @@ namespace Biblioteca.Models
 {
     public class UsuarioService
     {
-        public List<Usuario> Listar (){
-            using (BibliotecaContext bc = new BibliotecaContext()){
-               return bc.Usuarios.ToList();
+        public List<Usuario> Listar()
+        {
+            using (BibliotecaContext bc = new BibliotecaContext())
+            {
+                return bc.Usuarios.ToList();
+            }
         }
-    }
 
 
-        public  Usuario Listar (int id){
+        public Usuario Listar(int id)
+        {
 
-            using(BibliotecaContext bc = new BibliotecaContext()){
-               return bc.Usuarios.Find(id);
+            using (BibliotecaContext bc = new BibliotecaContext())
+            {
+                return bc.Usuarios.Find(id);
+            }
         }
-    }
 
-        public void incluirUsuario(Usuario novoUser){
+        public void incluirUsuario(Usuario novoUser)
+        {
 
-               using(BibliotecaContext bc = new BibliotecaContext()){
+            using (BibliotecaContext bc = new BibliotecaContext())
+            {
                 bc.Usuarios.Add(novoUser);
                 bc.SaveChanges();
             }
 
         }
 
-        public void editarUsuario(Usuario userEditado){
+        public void editarUsuario(Usuario userEditado)
+        {
 
-             using(BibliotecaContext bc = new BibliotecaContext()){
-               
-               Usuario u = bc.Usuarios.Find(userEditado.Id);
+            using (BibliotecaContext bc = new BibliotecaContext())
+            {
 
-               u.Login = userEditado.Login;
-               u.Senha = userEditado.Nome;
-               u.Nome = userEditado.Senha;
-               u.Tipo = userEditado.Tipo;
+                Usuario u = bc.Usuarios.Find(userEditado.Id);
 
-               bc.SaveChanges();
-        } 
-    }
+                u.Login = userEditado.Login;
+                u.Senha = userEditado.Nome;
+                u.Nome = userEditado.Senha;
+                u.Tipo = userEditado.Tipo;
 
-        public void excluirUsuario(int id ){
-            
-            using(BibliotecaContext bc = new BibliotecaContext()){
+                bc.SaveChanges();
+            }
+        }
+
+
+        public void excluirUsuario(int id)
+        {
+
+            using (BibliotecaContext bc = new BibliotecaContext())
+            {
                 bc.Usuarios.Remove(bc.Usuarios.Find(id));
                 bc.SaveChanges();
             }
